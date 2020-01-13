2c90f9fc481bd7ae209342e26456416837ec726d
Zach Latta <zach@zachlatta.com>
2020-01-13 11:35:55 -0800

    Create script.bash

---
+++ script.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Well... look who it is."
+
+sleep 1
+
+echo "..."
+
+sleep 1
+
+echo "What was your name again?"
+
+read NAME
+
+sleep 1
+
+echo "..."
+
+sleep 1
+
+echo "It's nice to meet you $NAME."
